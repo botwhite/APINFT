51d2e7564538f5880be965adbbbced2a828ef15f
botwhite <41837534+botwhite@users.noreply.github.com>
2022-03-19 13:27:25 -0400

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@
 var MAX_SUPPLY = null
 const cors = require('cors')
 const CONTRACT_ADDRESS = "0x01595cB3a6F9c496Dcacb3b094A5Fc4B46b9A4Cc"
-const PORT = 80
+const PORT = 81
 const IS_REVEALED = true
 url= 'https://bsc-dataseed2.binance.org/'
 const UNREVEALED_METADATA = {
